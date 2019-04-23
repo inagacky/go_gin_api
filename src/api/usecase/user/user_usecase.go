be240951d37411c0f5173677ebd18731a5fb4979
d-inagaki <chanc.euse.0302@gmail.com>
2019-04-24 00:02:56 +0900

    :bug: ユーザー作成でfirstNameが設定されないバグ修正

--- src/api/usecase/user/user_usecase.go
+++ src/api/usecase/user/user_usecase.go
@@ -17,7 +17,7 @@ type GetUserResponse struct {
 
 // ユーザー作成のリクエスト
 type CreateUserRequest struct {
-	FirstName string `json:"firstName binding:"required"`
+	FirstName string `json:"firstName" binding:"required"`
 	LastName  string `json:"lastName" binding:"required"`
 	Email     string `json:"email" binding:"required,email"`
 }
