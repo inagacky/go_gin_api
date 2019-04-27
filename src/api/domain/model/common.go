106084cf81456466bd701e85c1ed9a9d163d5482
d-inagaki <chanc.euse.0302@gmail.com>
2019-04-27 17:44:42 +0900

    recycle: modelの共通部分を切り出した

---
+++ src/api/domain/model/common.go
@@ -0,0 +1,13 @@
+package model
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"time"
+)
+
+type CommonModelFields struct {
+	Id          uint64     `gorm:"primary_key" json:"id"`
+	CreatedAt   time.Time  `json:"created_at"`
+	UpdatedAt   time.Time  `json:"updated_at"`
+	DeletedAt   *time.Time `json:"deleted_at"`
+}
