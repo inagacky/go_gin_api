23a061b940343a07841f0f536d7f9d5fcd871c20
d-inagaki <chanc.euse.0302@gmail.com>
2019-04-28 18:43:51 +0900

    :rotating_light: UTコードの追加

---
+++ src/api/usecase/user/user_usecase_test.go
@@ -0,0 +1,39 @@
+package usecase
+
+
+import (
+	"github.com/stretchr/testify/assert"
+	"strconv"
+	"testing"
+)
+
+func TestConvertUserModel(t *testing.T) {
+
+	// CreateUserリクエストのテスト
+	createUserRequest := CreateUserRequest{
+		FirstName:"TestFirstName",
+		LastName:"TestLastName",
+		Email:"test@gmail.com",
+	}
+
+	user := createUserRequest.ConvertUserModel()
+
+	assert.Equal(t, createUserRequest.FirstName, user.FirstName, "not Matched FirstName")
+	assert.Equal(t, createUserRequest.LastName, user.LastName, "not Matched LastName")
+	assert.Equal(t, createUserRequest.Email, user.Email, "not Matched Email")
+
+	// UpdateUserリクエストのテスト
+	updateUserRequest := UpdateUserRequest{
+		Id: "1",
+		FirstName:"TestFirstName",
+		LastName:"TestLastName",
+		Email:"test@gmail.com",
+	}
+	user = updateUserRequest.ConvertUserModel()
+
+	id, _ := strconv.ParseUint(updateUserRequest.Id, 10 ,64)
+	assert.Equal(t, id, user.Id, "not Matched Id")
+	assert.Equal(t, updateUserRequest.FirstName, user.FirstName, "not Matched FirstName")
+	assert.Equal(t, updateUserRequest.LastName, user.LastName, "not Matched LastName")
+	assert.Equal(t, updateUserRequest.Email, user.Email, "not Matched Email")
+}
